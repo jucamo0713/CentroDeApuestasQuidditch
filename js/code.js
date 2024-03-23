a4cb770e7965e884838b0bf09db29034bba17fcf
danielvillam <jvillame@unal.edu.co>
2024-03-23 10:30:12 -0500

    Actualización del front. Mejoras necesarias

---
+++ js/code.js
@@ -0,0 +1,11 @@
+function mostrarAlerta() {
+    alert("Debes estar registrado para hacer uso de esta función");
+}
+
+function aggFavorite() {
+    alert("Equipo agregado a favoritos con exito");
+}
+
+function upload() {
+    alert("Recarga realizada con exito");
+}
